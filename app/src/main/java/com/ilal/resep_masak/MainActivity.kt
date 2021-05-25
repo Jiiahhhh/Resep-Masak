81c996d2fa3280df2e1d8571d394e5a8b8b4f920
Muhammad Ijlal Nurhadi <ijlalnurhadi01@gmail.com>
2021-05-25 16:06:28 +0800

    Set Up MainActivity

--- app/src/main/java/com/ilal/resep_masak/MainActivity.kt
+++ app/src/main/java/com/ilal/resep_masak/MainActivity.kt
@@ -2,10 +2,21 @@ package com.ilal.resep_masak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.ilal.resep_masak.fragment.ResepFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment: Fragment = ResepFragment()
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
